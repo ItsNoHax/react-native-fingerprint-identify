ea920a5b6303aea8e2519c9ecb7b20785ec6237d
Alberto Blanco <itsnohax@gmail.com>
2017-11-24 14:14:09 +0100

    Remove unused imports

--- android/src/main/java/com/fingerprint/identify/RNFingerprintIdentifyModule.java
+++ android/src/main/java/com/fingerprint/identify/RNFingerprintIdentifyModule.java
@@ -6,14 +6,12 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import com.wei.android.lib.fingerprintidentify.FingerprintIdentify;
 import com.wei.android.lib.fingerprintidentify.base.BaseFingerprint;
 
-import android.content.Context;
 import android.app.Activity;
 import android.util.Log;
 
